0bd1f869469ba886ffd4af34712f1db0bba0f9db
Diogo "Mozão" Fonseca <devmozao@outlook.com>
2022-06-19 06:13:00 -0300

    feat: update commitlint config

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -6,6 +6,8 @@ const Configuration: UserConfig = {
     'type-empty': [2, 'never'],
     'type-case': [2, 'always', 'lower-case'],
     'subject-empty': [2, 'never'],
+    'body-empty': [2, 'always'],
+    'footer-empty': [2, 'always'],
   },
 }
 
