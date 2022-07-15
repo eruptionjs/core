106280699ca07a0d1c74d3d260c7d819c4f4425b
Diogo "Mozão" Fonseca <devmozao@outlook.com>
2022-07-15 00:26:26 -0300

    chore(#5): fix husky config and add promp-cli for commits

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
-
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
