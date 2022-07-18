893acc76bd4a3b5464882dacb268f0c94941379e
Diogo "Mozão" Fonseca <devmozao@outlook.com>
2022-07-18 00:11:01 -0300

    chore(#2): add reportWebVitals() to the project; fix npmrc to correct LTS version (16)

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client'
 
 import { App } from '@/App'
 
+import reportWebVitals from './reportWebVitals'
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+// eslint-disable-next-line no-console
+reportWebVitals(console.log)
