577841ec4f35c9e32b3537c19d87f772dadf5bd9
Diogo "Mozão" Fonseca <devmozao@outlook.com>
2022-08-24 19:46:51 -0300

    chore: update some config files only

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 
 import { App } from '@/App'
 
 import reportWebVitals from './reportWebVitals'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <App />
   </StrictMode>,
--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,15 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  envDir: './env',
   plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
     watch: false,
     environment: 'happy-dom',
     setupFiles: './src/setup-test.ts',
+  },
+  build: {
+    sourcemap: true,
   },
 })
