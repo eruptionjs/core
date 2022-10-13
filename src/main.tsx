101fb08540e3150ba72cbbfb791e8c0ba47f33e1
João Martins <jvmartinsdev@gmail.com>
2022-10-13 02:01:59 -0300

    feat: disabling react devtools in prod

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+
 import { App } from '@/App'
 
 import './index.css'
+
+if (import.meta.env.MODE === 'production') {
+  disableReactDevTools()
+}
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
